180a315c593058b5d49e4528888815c732848536
MaheshK0901 <maheshkumara0901@gmail.com>
2025-05-26 17:22:41 +0530

    Add new css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import History from "./pages/History";
